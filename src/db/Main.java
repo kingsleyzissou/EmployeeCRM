5e1924fd7ae2e39f4464508c0d62b903abdabcec
Gianluca <gianlucazuccarelli@gmail.com>
2020-10-14 20:31:04 +0100

    chore: remove commented lines

--- src/db/Main.java
+++ src/db/Main.java
@@ -24,54 +24,6 @@ public class Main {
 			return;
 		}
 
-		// Create a ResultSet & Display Table contents
-		// (Previously Create a table)
-		try {
-
-		    // Insert code here (from lecture notes)
-//			Statement s = conn.createStatement ();
-//			 s.executeUpdate ("DROP TABLE IF EXISTS animal");
-//			 s.executeUpdate (
-//			 "CREATE TABLE animal ("
-//			 + "id INT UNSIGNED NOT NULL AUTO_INCREMENT,"
-//			 + "PRIMARY KEY (id),"
-//			 + "name CHAR(40), category CHAR(40))");
-//			 int count;
-//			 count = s.executeUpdate (
-//			  "INSERT INTO animal (name, category)"
-//			  + " VALUES"
-//			  + "('snake', 'reptile'),"
-//			  + "('frog', 'amphibian'),"
-//			  + "('tuna', 'fish'),"
-//			  + "('racoon', 'mammal')");
-//			  s.close ();
-//			  System.out.println (count +
-//			  " rows were inserted");
-			Statement s = conn.createStatement ();
-			s.executeQuery ("SELECT id, name, category " +
-			 "FROM animal");
-			ResultSet rs = s.getResultSet ();
-			int count = 0;
-			while (rs.next ()) {
-				 int idVal = rs.getInt ("id");
-				 String nameVal = rs.getString ("name");
-				 String catVal = rs.getString ("category");
-				 System.out.println (
-				 "id = " + idVal
-				 + ", name = " + nameVal
-				 + ", category = " + catVal);
-				 ++count;
-			}
-			rs.close ();
-			s.close ();
-			System.out.println (count + " rows were retrieved");
-		    System.out.println("ResultSet printed");
-
-	    } catch (SQLException e) {
-			System.out.println("ERROR: Could not create the resultSet");
-			e.printStackTrace();
-			return;
-		}
 		
 	}
 }
