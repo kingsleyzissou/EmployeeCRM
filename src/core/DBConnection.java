ba90b418798aa6d7a51a8283c35f9f6ea966f60a
Gianluca <gianlucazuccarelli@gmail.com>
2020-10-23 22:25:52 +0100

    docs: add missing comments

--- src/core/DBConnection.java
+++ src/core/DBConnection.java
@@ -22,10 +22,17 @@ public class DBConnection {
 	/** The name of the database we are testing with (this default is installed with MySQL) */
 	private final static String dbName = "test";
 	
-	
 	private static Connection connection = null;
 	
+	/**
+	 * Singleton class so only one database
+	 * instance is created
+	 * 
+	 * @return db connection
+	 * @throws SQLException
+	 */
 	public static Connection getConnection() throws SQLException {
+		// create the connection if has not already been created
 		if (DBConnection.connection == null) {
 			
 			Properties connectionProps = new Properties();
@@ -41,6 +48,7 @@ public class DBConnection {
 			);
 		}
 		
+		// return the database connection
 		return DBConnection.connection;
 	}
 
